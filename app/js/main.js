7d4b6c8d2b95c2c84afb575f747965a43e1b088b
Zell Liew <zellwk@gmail.com>
2015-08-29 19:08:36 +0800

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  if ('testing' == 'testing') {
+    // Note: Cannot have console log in tests 
+    // or Karma will return an error
+    // console.log("testing");  
+  }
+});
+
+function add(int1, int2) {
+  return int1 + int2;
+}
+
