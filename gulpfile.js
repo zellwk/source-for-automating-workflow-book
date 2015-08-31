dd204fdaa77d11b23a2de8072b4f8a5b7dd3b22c
Zell Liew <zellwk@gmail.com>
2015-09-01 00:50:20 +0800

    detect env with Travis and throw errors

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ function customPlumber(errTitle) {
   if (process.env.CI) {
     return plumber({
       errorHandler: function(err) {
-        throw Error(gutil.colors.red(err.message));
+        throw Error(err.message);
       }
     });
   } else {    
